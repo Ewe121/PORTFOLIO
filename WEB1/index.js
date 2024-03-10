4be182544541176c323bfe1d278e1559d8894de2
Ewe121 <36973818+Ewe121@users.noreply.github.com>
2024-03-10 14:02:29 +0100

    Add files via upload

---
+++ WEB1/index.js
@@ -0,0 +1,18 @@
+// = assigning, equal operator
+
+// == comparison operator
+
+// === stricty equal to
+
+// != is not equal to
+
+// !== is not equal to
+
+// !=== strictly not equal to
+
+let x = 10;
+let y = 10;
+
+if(x == y){
+    console.log("you are good")
+}
